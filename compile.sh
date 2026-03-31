5a66046bca08062eeb2dfe81e4403f37df43da69
DaisukeDaisuke <17798680+DaisukeDaisuke@users.noreply.github.com>
2026-03-31 20:59:38 +0900

    Update PHP versions in compile.sh [ci skip]

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-PHP_VERSIONS=("8.1.34" "8.2.30" "8.3.29" "8.4.16" "8.5.0")
+PHP_VERSIONS=("8.1.34" "8.2.30" "8.3.30" "8.4.19" "8.5.4")
 
 #### NOTE: Tags with "v" prefixes behave weirdly in the GitHub API. They'll be stripped in some places but not others.
 #### Use commit hashes to avoid this.
